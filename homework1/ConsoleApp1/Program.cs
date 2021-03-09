19c7fb9f9a912557e880391e5fcd273b8302223f
CheNy1210 <80326268+CheNy1210@users.noreply.github.com>
2021-03-09 23:11:51 +0800

    Add files via upload

---
+++ homework1/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+            double a = 0;
+            double b = 0;
+            Console.Write("Please input a double number:");
+            s = Console.ReadLine();
+            a = Double.Parse(s);
+            Console.Write("Please input another double number:");
+            s = Console.ReadLine();
+            b = Double.Parse(s);
+            Console.Write("Please input a operator:");
+            s = Console.ReadLine();
+            switch(s)
+            {
+                case "+":
+                    Console.WriteLine($"a+b:{a + b}");
+                    break;
+                case "-":
+                    Console.WriteLine($"a-b:{a - b}");
+                    break;
+                case "*":
+                    Console.WriteLine($"a*b:{a * b}");
+                    break;
+                case "/":
+                    Console.WriteLine($"a/b:{a / b}");
+                    break;
+            }
+            
+        }
+    }
+}
---
+++ homework1/windows2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace windows2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string m = textBox1.Text;
+            string n = textBox2.Text;
+            double a = Double.Parse(m);
+            double b = Double.Parse(n);
+            int s = comboBox1.SelectedIndex;
+            switch (s)
+            {
+                case 0:
+                    label5.Text = $"{a + b}";
+                    break;
+                case 1:
+                    label5.Text = $"{a - b}";
+                    break;
+                case 2:
+                    label5.Text = $"{a * b}";
+                    break;
+                case 3:
+                    label5.Text = $"{a / b}";
+                    break;
+            }
+        }
+       
+    }
+}
