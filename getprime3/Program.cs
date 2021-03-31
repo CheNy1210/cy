186d7608451b02205d417d0ef4b8f2f9a44ebace
CheNy1210 <80326268+CheNy1210@users.noreply.github.com>
2021-03-31 11:14:28 +0800

    Add files via upload

---
+++ getprime3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace getprime3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2-100的所有素数为：");
+            int i = 2;
+            for (i = 2; i <= 100; i++)
+            {
+                if (IsPrimeNumber(i))
+                    Console.Write(i+",");
+            }
+        }
+        public static bool IsPrimeNumber(int n)
+        {
+            int i;
+            bool c = true;
+            for (i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                    c = false;
+            }
+            return c;
+        }
+    }
+}
